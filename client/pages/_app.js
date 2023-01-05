f7c67de18242c97dd67c41eb30efa8b94e6a05f4
Thang Za Thang <Thangzathang@gmail.com>
2023-01-06 10:52:33 +1100

    initial commit

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import NavbarComponent from "../components/NavbarComponent";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavbarComponent />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,9 +4,11 @@ module.exports = {
     //
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    // Flowbite
+    "./node_modules/flowbite-react/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
