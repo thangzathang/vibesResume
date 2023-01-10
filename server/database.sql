3348ee28c762e279d0df7da053d9d5cfdba3ea4c
Thang Za Thang <Thangzathang@gmail.com>
2023-01-10 14:30:40 +1100

    jwt n bcrypt added to project n users table added

--- server/database.sql
+++ server/database.sql
@@ -1,10 +1,21 @@
 CREATE DATABASE movielist;
 
+-- Create Movie 
 CREATE TABLE movies(
     movie_id SERIAL PRIMARY KEY,
     movie_name VARCHAR(255),
     movie_description VARCHAR(255),
     movie_rating INT
+);
+
+-- set extension
+-- Create Users
+CREATE TABLE users(
+    user_id uuid PRIMARY KEY DEFAULT 
+    uuid_generate_v4(),
+    user_name VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL
 );
 
 -- Adding New Columns to the movie table in the database movieList
