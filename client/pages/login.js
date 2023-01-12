27fd0d4a7d1615cde03f910862fbfb8aff7539ce
Thang Za Thang <Thangzathang@gmail.com>
2023-01-12 12:51:53 +1100

    Login and register

---
+++ client/pages/login.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+
+// Toast
+import { toast } from "react-toastify";
+
+const formStyle = {
+  display: "flex",
+  flexDirection: "column",
+  maxWidth: "560px",
+};
+
+const Login = () => {
+  // Router
+  const router = useRouter();
+
+  // State
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  // Handle Submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const body = { password, email };
+
+      const response = await fetch("http://localhost:5000/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      const parseResponse = await response.json();
+      // console.log("Data we get back:", parseResponse);
+      if (parseResponse.token) {
+        localStorage.setItem("token", parseResponse.token);
+
+        toast("Successfully Logged In!");
+        router.push("/moviesPage");
+        return;
+      } else {
+        toast(parseResponse.message);
+      }
+    } catch (error) {
+      console.log("Error at Logging in: login.js", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form style={formStyle} onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor="email"> Email </label>
+        <input type="text" name="email" id="email" onChange={(e) => setEmail(e.target.value)} />
+
+        <label htmlFor="password"> Password </label>
+        <input type="text" name="password" id="password" onChange={(e) => setPassword(e.target.value)} />
+
+        <button type="submit"> Login </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ client/pages/register.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+
+// Toast
+import { toast } from "react-toastify";
+
+const formStyle = {
+  display: "flex",
+  flexDirection: "column",
+  maxWidth: "560px",
+};
+
+const Register = () => {
+  // Router
+  const router = useRouter();
+
+  // State
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  // Message
+  const [message, setMessage] = useState("");
+
+  // Handle Submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const body = { username, password, email };
+
+      const response = await fetch("http://localhost:5000/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      const parseResponse = await response.json();
+      // console.log("Data we get back:", parseResponse);
+      if (parseResponse.token) {
+        localStorage.setItem("token", parseResponse.token);
+        // setMessage("User created!");
+        toast("User created!");
+        router.push("/moviesPage");
+        return;
+      } else {
+        // setMessage(parseResponse.message);
+        toast(parseResponse.message);
+      }
+    } catch (error) {
+      console.log("Error at Register: register.js", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>{message}</h1>
+
+      <h1>Register</h1>
+
+      <form style={formStyle} onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor="username"> Username </label>
+        <input type="text" name="username" id="username" onChange={(e) => setUsername(e.target.value)} />
+
+        <label htmlFor="email"> Email </label>
+        <input type="text" name="email" id="email" onChange={(e) => setEmail(e.target.value)} />
+
+        <label htmlFor="password"> Password </label>
+        <input type="text" name="password" id="password" onChange={(e) => setPassword(e.target.value)} />
+
+        <button type="submit" className="bg-green-500 p-8 m-4">
+          {" "}
+          Register{" "}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
