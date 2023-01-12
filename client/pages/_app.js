583ee1528e1dc7e021b1995631f17b3d3820c79e
Thang Za Thang <Thangzathang@gmail.com>
2023-01-12 12:50:27 +1100

    toast added to front end app

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,21 @@
 import "../styles/globals.css";
+import { useState } from "react";
 
+// Component
 import NavbarComponent from "../components/NavbarComponent";
 
+// Toastify
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export default function App({ Component, pageProps }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <>
       <NavbarComponent />
+
+      <ToastContainer />
 
       <Component {...pageProps} />
     </>
