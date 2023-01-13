c8b5c344b27241a04853e77fa76d38f2cd94f095
Thang Za Thang <Thangzathang@gmail.com>
2023-01-13 11:03:14 +1100

    next js update

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // Component
 import NavbarComponent from "../components/NavbarComponent";
@@ -10,6 +10,26 @@ import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  async function checkAuth() {
+    try {
+      const response = await fetch("http://localhost:5000/auth/verify", {
+        method: "GET",
+        headers: { token: localStorage.token },
+      });
+
+      const parseResponse = await response.json();
+      console.log("Auth Server says:", parseResponse);
+    } catch (error) {
+      console.log("Check auth error:", error);
+    }
+  }
+
+  // Check if user authenticated
+  useEffect(() => {
+    // verify json token
+    checkAuth();
+  }, []);
 
   return (
     <>
