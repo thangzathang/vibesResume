64c544fe65dae6fecc4426ccb7e8373230300305
Thang Za Thang <Thangzathang@gmail.com>
2023-01-30 12:06:30 +1100

    fix

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,7 +5,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   let show = process.env.NEXT_PUBLIC_PRODUCTION;
   return (
-    <div className="bg-gray-900 min-h-screen">
+    <div className="bg-gray-900 min-h-screen text-white">
       <small>
         You are running this application in <b>{show}</b> mode.
       </small>
