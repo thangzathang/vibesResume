fcbd6ae6e5e34903bca99a5bab974ce6ec99a3e1
Thang Za Thang <Thangzathang@gmail.com>
2023-02-02 14:45:11 +1100

    md fix 5

--- client/middleware.js
+++ client/middleware.js
@@ -5,28 +5,52 @@ import { NextRequest } from "next/server";
 
 export async function middleware(request) {
   const token = await request.cookies.get("token")?.value;
-  console.log("Token:", token);
-  const { pathname } = request.nextUrl;
 
+  // console.log("Token is 0:", token === 0);
+  // console.log("Token is false:", token === false);
+  // console.log("Token is undefined:", token === undefined);
+  // console.log("Token is length:", token.length);
+  // console.log("Token is length 0:", token.length === 0);
+
+  // const { pathname } = request.nextUrl;
+
+  // if (request.url.includes("/moviesPage")) {
+  //   if (token.length === 0) {
+  //     const url = request.nextUrl.clone();
+  //     url.pathname = "/auth/login";
+  //     return NextResponse.rewrite(url);
+  //   }
+
+  //   try {
+  //     return NextResponse.next();
+  //   } catch (error) {
+  //     return NextResponse.redirect("/auth/login");
+  //   }
+  // }
+
+  // return NextResponse.next();
+
+  // My attempt 1
   if (request.url.includes("/auth/login")) {
     return NextResponse.next();
   }
 
-  if (request.url.includes("/moviesPage" && token)) {
+  if (request.url.includes("/moviesPage" && token !== 0)) {
     return NextResponse.next();
   }
 
-  // if (request.url.includes("/moviesPage") && !token) {
-  //   const url = request.nextUrl.clone();
-  //   url.pathname = "/auth/login";
-  //   return NextResponse.rewrite(url);
-  // }
-
-  if (!token) {
+  if (request.url.includes("/moviesPage") && token.length === 0) {
     const url = request.nextUrl.clone();
     url.pathname = "/auth/login";
     return NextResponse.rewrite(url);
   }
+
+  // Infinite loop - dont use
+  // if (!token) {
+  //   const url = request.nextUrl.clone();
+  //   url.pathname = "/auth/login";
+  //   return NextResponse.rewrite(url);
+  // }
 }
 
 // See "Matching Paths" below to learn more
