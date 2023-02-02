ad952dcb98d8aaffa0f9cacbfb95131a8dafb746
Thang Za Thang <Thangzathang@gmail.com>
2023-02-02 15:15:23 +1100

    md fix 13

--- client/middleware.js
+++ client/middleware.js
@@ -42,7 +42,7 @@ export async function middleware(request) {
   // }
 
   // Infinite loop - dont use
-  if (token ? token.length === 0 : false && request.url.includes("/moviesPage")) {
+  if (token ? token.length === 0 : true && request.url.includes("/moviesPage")) {
     const url = request.nextUrl.clone();
     url.pathname = "/auth/login";
     return NextResponse.rewrite(url);
